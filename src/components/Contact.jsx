63fa4716e5f408e207bd9103ca8e175498bd90f3
Pranav Modgil <pranavmodgil@gmail.com>
2023-08-14 00:36:32 +0530

    Added Email functionality but it is still not working

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,7 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -31,18 +32,19 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
+
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_ykzv3hl',
+        'template_5fam2af ',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Pranav Modgil",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "pranavmodgil@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        '-HtQeFEDk0z4m7qFA'
       )
       .then(
         () => {
